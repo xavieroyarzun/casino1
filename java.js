775914661fcb129b7e2f91f89e3beb0f23bc1dc6
Santibnaez <javier.santibanez1@mail.udp.cl>
2025-09-01 22:48:07 -0400

    De acuerdo al plano que hicimos:

--- java.js
+++ java.js
@@ -3,7 +3,7 @@ let btI=document.getElementById("izquierdaboto")
 let btD=document.getElementById("derechaboto")
 
 
-let imagenes = ["Img\casino img 1.jpg", "Img\casino img 2.jpg", "Img\casino img 3.jpg"];
+let imagenes = ["Img/casino img 1.jpg", "Img/casino img 2.jpg", "Img/casino img 3.jpg"];
   let index = 0; // empieza en la primera imagen
 
   function mostrarImagen() {
